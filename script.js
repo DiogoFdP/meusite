c7f33cafdf5e3b1ac81ba03934b91cd3232eb051
DiogoFdP <dgfernandes816@gmail.com>
2026-04-13 11:01:52 -0300

    de

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 // 1. CONFIGURAÇÃO DO FIREBASE
 // --------------------------------------------------------
 const firebaseConfig = {
-    apiKey: "COLE_SUA_API_KEY_AQUI",
-    authDomain: "COLE_SEU_AUTH_DOMAIN_AQUI",
-    projectId: "COLE_SEU_PROJECT_ID_AQUI",
-    storageBucket: "COLE_SEU_STORAGE_BUCKET_AQUI",
-    messagingSenderId: "COLE_SEU_MESSAGING_SENDER_ID_AQUI",
-    appId: "COLE_SEU_APP_ID_AQUI"
+  apiKey: "AIzaSyB4GuLtPZML1HL-YHBZOqEFi0oVbhdgmFs",
+  authDomain: "douglas-site-fec83.firebaseapp.com",
+  projectId: "douglas-site-fec83",
+  storageBucket: "douglas-site-fec83.firebasestorage.app",
+  messagingSenderId: "919058405326",
+  appId: "1:919058405326:web:d5118aa27679dc1cfdfd9f"
 };
 
 firebase.initializeApp(firebaseConfig);
